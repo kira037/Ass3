666191fb7ce96432af8526da2c7414662922b226
Kerou Mao <keroum@student.unimelb.edu.au>
2022-05-27 23:22:38 +1000

    section2fixed

--- navbar.js
+++ navbar.js
@@ -1,5 +1,5 @@
 /* responsive" topnav when the user clicks on the icon */
-function myFunction() {
+function responsiveNav() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
       x.className += " responsive";
