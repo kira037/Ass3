585a6bc61ffbff3518142ae8ee3330154b389dd1
Kerou Mao <keroum@student.unimelb.edu.au>
2022-05-29 22:14:21 +1000

    reusable header and footer

---
+++ homepage.js
@@ -0,0 +1,29 @@
+/*learn more button js*/
+function viewFunction(){
+  document.getElementsByClassName('view').onclick=location.href ="product list.html"
+}
+
+/*learn more button js*/
+function cartFunction(){
+  document.getElementsByClassName('cart').onclick=location.href ="cart.html"
+}
+
+
+/* scroll to vr */
+function vrFunction() {
+  document.getElementsByClassName('forth-section').scrollIntoView({ behavior: 'smooth', block: 'center' })
+};
+
+
+/* scroll to vr */
+$(".link_to_vr").click(function() {
+  $('html,body').animate({
+      scrollTop: $(".vr-section").offset().top},
+      'slow');
+});
+
+/* return to top */
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
--- navbar.js
+++
@@ -1,9 +0,0 @@
-/* responsive" topnav when the user clicks on the icon */
-function responsiveNav() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
