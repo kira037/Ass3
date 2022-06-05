49df012e72b61b39a770e48b2cfa179ebf15dde1
Kerou Mao <keroum@student.unimelb.edu.au>
2022-06-05 18:04:53 +1000

    cart

---
+++ product-page.js
@@ -0,0 +1,26 @@
+$('.content-link button').click(function () {
+    var idx = $(this).closest('.content-link').index();
+    
+    $('.content-link button').removeClass('active-link');
+    $(this).addClass('active-link');
+
+    $('.content-panel').hide();
+    $('.content-panel').eq(idx).show();
+});
+
+
+/* Assign actions */
+$('.product-quantity input').change( function() {
+    updateQuantity(this);
+  });
+
+
+  function changeVal(el) {
+  var qt = parseFloat(el.parent().children(".qt").html());
+  var price = parseFloat(el.parent().children(".price").html());
+  var eq = Math.round(price * qt * 100) / 100;
+  
+  el.parent().children(".full-price").html( eq + "€" );
+  
+  changeTotal();      
+}
